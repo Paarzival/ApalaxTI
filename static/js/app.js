0f1ef8d61814afeed88e5599327ab5a29ff61a94
paarzival <paarzival1@gmail.com>
2017-10-19 18:44:03 -0500

    se agrega pagina de inicio

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+
+
+    $(function() {
+	    $('#dinamic_text').typeIt({
+		     strings: ["tu conocimiento", "tus habilidades","tus metas"],
+		     speed: 50,
+		     breakLines: false,
+		     autoStart: false,
+		     loop: true
+		});
+    });
